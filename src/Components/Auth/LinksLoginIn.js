9bb92c9ad06bc3d802519f2571a1b6af68b50bed
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2020-01-04 05:35:23 +0100

    Initials Working :)

--- src/Components/Auth/LinksLoginIn.js
+++ src/Components/Auth/LinksLoginIn.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/authActions';
 
 const LinksLoginIn = (props) =>{
+    const {profile} = props;
     
         return (
             <div className="LinksIn">
@@ -11,6 +12,7 @@ const LinksLoginIn = (props) =>{
                  <li><NavLink to="/createproject">Zlecenie Naprawy!</NavLink></li> 
                  <li><button className="btn btn-danger" onClick={props.signOut}>Wyloguj się!</button></li> 
                  <li><NavLink to="/contact">Menu</NavLink></li>
+                 <li>{profile.initials}</li>
                  
                </ul> 
             </div>
--- src/Components/Layaut/Navbar.js
+++ src/Components/Layaut/Navbar.js
@@ -5,8 +5,8 @@ import LinksLoginOut from '../Auth/LinksLoginOut';
 import { connect } from 'react-redux';
 
 const Navbar = (props) =>{
-   const {auth} = props;
-   const links = auth.uid ? <LinksLoginIn/> : <LinksLoginOut/> ;
+   const {auth, profile} = props;
+   const links = auth.uid ? <LinksLoginIn profile={profile}/> : <LinksLoginOut/> ;
     return(
         <div className="NavbarBasi">
                 <nav className="nav-wrapper">
@@ -20,7 +20,8 @@ const Navbar = (props) =>{
 const mapStateToProps = (state) => {
     console.log(state);
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
        
     }
 }
