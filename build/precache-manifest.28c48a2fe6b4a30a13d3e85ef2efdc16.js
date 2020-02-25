81ec426aaa78ba063ccabf6f70c5f48e476d12ab
BasakKamil <Bastric91@gmail.com>
2020-02-25 22:49:36 +0100

    Sendit

---
+++ build/precache-manifest.28c48a2fe6b4a30a13d3e85ef2efdc16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564385957f98eaaf7be0d65eb8edf623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011fc7c5039c07902bf9",
+    "url": "/static/css/main.03c9b633.chunk.css"
+  },
+  {
+    "revision": "37f0933be95fee54f1b1",
+    "url": "/static/js/2.4692c613.chunk.js"
+  },
+  {
+    "revision": "0b4aea322e6149e84fedfa21b9fca8e3",
+    "url": "/static/js/2.4692c613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011fc7c5039c07902bf9",
+    "url": "/static/js/main.13d52d64.chunk.js"
+  },
+  {
+    "revision": "ba03e2f36ee68c372e65",
+    "url": "/static/js/runtime~main.b6b5bdc4.js"
+  },
+  {
+    "revision": "40c94b4574538c3c0acf9103f78c717d",
+    "url": "/static/media/amman.40c94b45.jpg"
+  },
+  {
+    "revision": "2a473e875f6c981e5a9df1f5f0207370",
+    "url": "/static/media/oskar.2a473e87.jpg"
+  },
+  {
+    "revision": "b9747645c4797243c6a16e5bf1fba6e3",
+    "url": "/static/media/slub.b9747645.jpg"
+  },
+  {
+    "revision": "f579e3e0b81f8ea132905f4dfa7824bc",
+    "url": "/static/media/smoke1.f579e3e0.png"
+  },
+  {
+    "revision": "94cd6863a11676e5c5cd210b2103e047",
+    "url": "/static/media/soprano.94cd6863.png"
+  },
+  {
+    "revision": "0dd07e94f7a1d76daf3592d83ded1271",
+    "url": "/static/media/star.0dd07e94.png"
+  }
+]);
